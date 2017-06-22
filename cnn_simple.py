af9a70cc9e4d73418b646d036b7a10a6ff15c85f
Brent Runyan <brent.runyan@micro-dimensions.com>
2017-06-22 11:29:12 +0200

    Added comments and documentation.

--- cnn_simple.py
+++ cnn_simple.py
@@ -1,3 +1,13 @@
+'''
+This script builds and trains a simple convolutional neural network (CNN) and trains it to detect tumors in histology images.
+
+The network is adapted from the one used to recognize the MNIST data set from the Tensorflow tutorial:
+https://www.tensorflow.org/get_started/mnist/pros
+
+The histology image data is taken from that provided for the CAMELYON16 challenge:
+https://camelyon16.grand-challenge.org/
+'''
+
 import math
 import numpy as np
 import os
@@ -5,38 +15,54 @@ import random
 import tensorflow as tf
 from PIL import Image 
 
+# Setting TF_CPP_MIN_LOG_LEVEL to 2 suppresses warnings, such as those that report that Tensorflow was not compiled with SSE support
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 sess = tf.InteractiveSession()
 
-# x = tf.placeholder(tf.float32, shape=[None, 784])
+
+##---------------------------------
+## Construct the network
+##---------------------------------
+
+# placeholder for input tensor: [num_images, image_width_px, image_height_px, num_image_channels]
 x_image = tf.placeholder(tf.float32, shape=[None, 28, 28, 3])
+# placeholder for output variable: [num_images_classified, num_classes]
+#   in our case we have two classes:
+#     tumor...: [1 0]
+#     no tumor: [0 1]
 y_ = tf.placeholder(tf.float32, shape=[None, 2])
 
-
+# generate a randomized initial weight tensor
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
 
+# generate a constant initial bias tensor
 def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
 
-# stride of 1 and zero-padded, so the output size is the same as the input size
+# x is the input tensor: [batch, in_height, in_width, in_channels]
+# W is the filter: [filter_height, filter_width, in_channels, out_channels]
+# returns the convolved tensor with the same widht and height as the inpu tensor, 
+#    since the stride is 1 and zero-padding is used
+# see https://www.tensorflow.org/api_docs/python/tf/nn/conv2d for more details
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
 
+# x is the input tensor: [batch, in_height, in_width, in_channels] 
+# retrns a the 2x2 max=pooled tensor with half the heigh abd width of the input tensor
+# see https://www.tensorflow.org/api_docs/python/tf/nn/max_pool for more information
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 # Layer 1
 #   convolution: 32 features for each 5x5 patch
 #   max pooling:
-
 W_conv1 = weight_variable([5, 5, 3, 32]) # patch size, num input channels, num output channels
 b_conv1 = bias_variable([32]) # one component for each output channel
 
-#x_image = tf.reshape(x, [-1,28,28,1]) # ?, image width, image height, num color channels
 
 # convolve x_image with weight tensor, add bias, apply ReLU, and finally max pool
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # 28x28x32
@@ -72,16 +98,19 @@ b_fc2 = bias_variable([2])
 
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-##
-## Train and evaluate the model
-##
+
+
+
+##---------------------------------
+## Train and evaluate the network
+##---------------------------------
 
 cross_entropy = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv) )
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run( tf.global_variables_initializer() )
+
 
 def tile_image_as_ndarray(filename):
   tile = Image.open(filename)
@@ -127,6 +156,8 @@ def generate_training_test_data( res ):
 
 training_data, testing_data = generate_training_test_data(512)
 
+sess.run( tf.global_variables_initializer() )
+
 # train
 batch_size = 50
 for i in range(0, len(training_data), batch_size):
@@ -139,10 +170,8 @@ for i in range(0, len(training_data), batch_size):
     print("step %d, training accuracy %g"%(i, train_accuracy))
    train_step.run(feed_dict={x_image:images, y_:targets, keep_prob:0.5})
 
-# test 
+# evaluate 
 batch0, batch1 = zip(*testing_data)
 test_images = np.array(batch0)
 test_targets = np.reshape(np.array(batch1), [-1,2])
 print("test accuracy %g"%accuracy.eval(feed_dict={x_image: test_images, y_:test_targets, keep_prob:1.0}))
-
-# make sure to save results, especially if it takes 40 min to generat them
